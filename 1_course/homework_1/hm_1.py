d6b63a89f36bfe577168b723d119a3cfeceea6ad
andreiDmitriev2022 <107244116+andreiDmitriev2022@users.noreply.github.com>
2023-07-28 00:49:21 +0500

    Циклы и списки

--- 1_course/homework_1/hm_1.py
+++ 1_course/homework_1/hm_1.py
@@ -1,52 +1,58 @@
 # 1. Приветствие
-print("Привет! Предлагаю проверить свои знания английского!")
 
-# 2. Считаем имя пользователя
-user_name =  input("Расскажи как тебя зовут!")
+start = input("Привет! Предлагаю проверить свои знания английского! Наберите ready, чтобы начать!")
 
-# 3. Выведем имя пользователя
+if start == "ready":
+    user_name =  input("Расскажи как тебя зовут!")
 
-print(f"Привет, {user_name}, начинаем тренировку!")
+    # 3. Выведем имя пользователя
 
-# 4. Иницилизация вспомогательных переменных
-balls = 0
-right_a = 'is'
+    print(f"Привет, { user_name }, начинаем тренировку!")
 
-# 5. Задаем вопросы
-quest = input("My name ___ Vova")
+    # 4. Иницилизация вспомогательных переменных
+    balls = 0
 
-if quest == right_a:
-    print("Ответ верный!")
-    print("Вы получаете 10 баллов!")
-    balls += 10
+    questions = ["My name ___ Vova", "I  ___ a coder", "I live ___ Moscow"]
+    answers=["is", "am", "in"]
+    count_questions = len(questions)
+
+    # 5. Задаем вопросы
+    for q in range(count_questions):
+        attempt = 1
+        while True:
+            quest = input(questions[q])
+
+            if quest == answers[q]:
+                print("Ответ верный!")
+
+                if attempt == 1:
+                    balls += 3
+
+                elif attempt == 2:
+                    balls += 2
+                
+                elif attempt == 3:
+                    balls += 1
+                break
+            else:
+                if attempt < 3:
+                    attempt += 1 
+                    print(f"Осталось попыток: { 4 - attempt }, попробуйте ещё раз!")
+
+                else:
+                    print(f"Увы, но нет! Верный ответ: { answers[q] }")
+                    break
+        
+
+    result_percents = round( ( balls / ( count_questions * 10 ) ) * 100, 2 )
+        
+    # вывод результатов
+    print(
+        f"Вот и всё, { user_name }! " + 
+        f"Вы ответили на { int(balls / count_questions) } вопросов из { count_questions } верно, " +
+        f"это { result_percents } процентов."
+    )
 else:
-    print("Неправильно.")
-    print(f"Правильный ответ: {right_a}")
+    print("Кажется, вы не хотите играть. Очень жаль.")
 
-right_a = 'am'
-quest = input("I  ___ a coder")
 
-if quest == right_a:
-    print("Ответ верный!")
-    print("Вы получаете 10 баллов!")
-    balls += 10
-else:
-    print("Неправильно.")
-    print(f"Правильный ответ: {right_a}")
-    
-right_a = 'in'
-quest = input("I live ___ Moscow")
-
-if quest == right_a:
-    print("Ответ верный!")
-    print("Вы получаете 10 баллов!")
-    balls += 10
-else:
-    print("Неправильно.")
-    print(f"Правильный ответ: {right_a}")
-    
-# вывод результатов
-print(f"Вот и всё, {user_name}!")
-print(f"Вы ответили на { int(balls / 10) } вопросов из 3 верно.")
-print(f"Вы заработали {balls} баллов.")
-print(f"Это { (balls / 30) * 100 } процентов.")
